a811096d54da387dd2464c9b4c398fe77d2b596f
RachelKolk <rach.m.obrien@gmail.com>
2019-02-19 16:03:13 -0500

    get and get by id tested and working

--- index.js
+++ index.js
@@ -20,7 +20,36 @@ server.use(helmet());
 
 // endpoints here
 
-const port = 3300;
+server.post('/zoos')
+
+server.get('/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.get('/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos')
+    .where({id: req.params.id})
+    .first();
+
+    if (zoo) {
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: "Zoo not found"});
+    }
+    
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+const port = process.env.PORT || 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
