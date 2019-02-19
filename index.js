7b2ba7009b69772124ccf82c301877cbc6e4836d
RachelKolk <rach.m.obrien@gmail.com>
2019-02-19 16:36:09 -0500

    post and put coded still need to test

--- index.js
+++ index.js
@@ -20,9 +20,26 @@ server.use(helmet());
 
 // endpoints here
 
-server.post('/zoos')
+server.post('api/zoos', async (req, res) => {
+  try{
+    const [id] = await db('zoos')
+    .insert(req.body);
+    const zoo = await db('zoos')
+    .where({id})
+    .first();
+    res.status(201).json(zoo);
+      // if (req.body == '' || req.body == null) {
+      //   res.status(404).json({message: 'Please fill in the zoo name'});
+      // } else {
+      //   res.status(201).json(zoo);
+      // }
+    
+  } catch (error) {
+      res.status(500).json({error: 'There was an error adding a zoo'});
+  }
+});
 
-server.get('/zoos', async (req, res) => {
+server.get('/api/zoos', async (req, res) => {
   try {
     const zoos = await db('zoos');
     res.status(200).json(zoos);
@@ -31,7 +48,7 @@ server.get('/zoos', async (req, res) => {
   }
 });
 
-server.get('/zoos/:id', async (req, res) => {
+server.get('/api/zoos/:id', async (req, res) => {
   try {
     const zoo = await db('zoos')
     .where({id: req.params.id})
@@ -45,6 +62,25 @@ server.get('/zoos/:id', async (req, res) => {
     
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+server.put('api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+    .where({id: req.params.id})
+    .update(req.body);
+
+    if (count > 0) {
+      const zoo = await db('zoos')
+      .where({id: req.params.id})
+      .first();
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({message: 'That zoo is not in our database'});
+    }
+  } catch (error) {
+      res.status(500).json({error: 'Something has gone terribly wrong'});
   }
 });
 
